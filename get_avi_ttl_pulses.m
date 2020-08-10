b5a4498001a63eaff467b65ce85024810733fa5e
Unknown <maimon.rose@gmail.com>
2020-08-10 10:47:03 -0700

    update for corrupted files

--- get_avi_ttl_pulses.m
+++ get_avi_ttl_pulses.m
@@ -16,7 +16,17 @@ total_samples_by_file = zeros(1,length(wav_files));
 
 for w = 1:max(wav_file_nums) % run through all requested .WAV files and extract audio data and TTL status at each sample
     if ismember(w,wav_file_nums)
-        data = audioread(fullfile(audio_dir,wav_files(w).name)); % load audio data
+        try
+            data = audioread(fullfile(audio_dir,wav_files(w).name)); % load audio data
+        catch err
+            if strcmp(err.identifier,'MATLAB:audiovideo:audioread:Unexpected')
+               disp('Corrupted File!')
+               total_samples_by_file(w) = 0;
+               continue
+            else
+                rethrow(err)
+            end
+        end
         ttl_status = bitand(data*wav2bit_factor + wav2bit_factor,1); % read TTL status off least significant bit of data
         audio_time_din = [audio_time_din (1e3*(total_samples + find(sign(diff(ttl_status))~=0)')/fs_wav)]; %#ok<AGROW>
         total_samples_by_file(w) = length(data);
